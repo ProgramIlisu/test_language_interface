8c69b24fa19138188db6ab91e4afcfd6ca53a8c7
ProgramIlisu <139325987+ProgramIlisu@users.noreply.github.com>
2025-11-18 19:15:07 +0300

    Update test_items.py

--- test_language_interface/test_items.py
+++ test_language_interface/test_items.py
@@ -4,9 +4,10 @@ def test_product_page_has_add_to_cart_button(browser):
     url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
     browser.get(url)
 
-    # Визуальная проверка языка — по заданию Stepik
+    # Визуальная проверка языка 
     time.sleep(3)
 
     button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     time.sleep(2)
+
     assert len(button) > 0, "Кнопка добавления в корзину НЕ найдена!"
